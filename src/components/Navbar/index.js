7a01713c54ff2c82cc65c1c19e217970a0e92a9e
ProgramingspaceT-800 15 <andreiaaraujodemelo@gmail.com>
2023-12-28 15:01:04 +0000

    feat: links

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -18,8 +18,8 @@ const Navbar = () => {
             <div className="logo">
               <img src="https://github.com/ProgramingspaceT-800/3CDisc/blob/main/public/logo%20(1).png?raw=true" alt="PH Negócios Plataforma" style={{ width: '100px', height: 'auto', marginTop: '5%', marginBottom: '3%' }} />
               <div className='link'>
-                <li><a href="/">Bases</a></li>
-                <li className="center"><a href="#">Graficos</a></li>
+                <li><a href="3-c-disc.vercel.app">Bases</a></li>
+                <li className="center"><a href="https://growth-stage-phi.vercel.app/">Graficos</a></li>
                 <li className="upward"><a href="#">Adicionar</a></li>
                 <li className="forward"><a href="#">Update</a></li>
               </div>
